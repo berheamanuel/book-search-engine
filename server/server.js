b4399552884fb53d9c8b7293c1045df32d985c7a
Amanuel <amanueleman@gmail.com>
2024-03-13 23:20:32 -0400

    refactoring the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
-const { expressMiddleware } = require("apollo-server-express");
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require("path");
 const { authMiddleware } = require("./utils/auth");
 
@@ -8,12 +8,12 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const routes = require("./routes");
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  // context: authMiddleware,
 });
 
 const startApolloServer = async () => {
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
