3a7565eed84545d16ffbbb073227f1575cf84afd
Amanuel <amanueleman@gmail.com>
2024-03-18 23:16:40 -0400

    refactoring and removing unwanted code

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,18 +12,18 @@ module.exports = {
     },
   }),
   // function for our authenticated routes
-  authMiddleware: function (req) {
+  authMiddleware: function ({ req } ) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      // console.log('req.header.authorization', req.headers.authorization);
+      console.log('req.header.authorization', req.headers.authorization);
       token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: "You have no token!" });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -32,11 +32,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log("Invalid token");
-      return res.status(400).json({ message: "invalid token!" });
+      
     }
     return req;
-    // send to next endpoint
-    // next();
+    
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
